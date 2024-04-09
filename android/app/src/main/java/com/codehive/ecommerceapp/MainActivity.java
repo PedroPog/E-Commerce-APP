d2ae45e39aa634796cb310815c99795420f15930
Pedro Vieira <80628417+PedroPog@users.noreply.github.com>
2024-04-09 13:25:57 -0300

    Incialiação do projeto e configuração de thema e directorios

---
+++ android/app/src/main/java/com/codehive/ecommerceapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.codehive.ecommerceapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,24 @@
+import 'package:ecommerceapp/utils/theme/custom_theme/text_theme.dart';
+import 'package:flutter/material.dart';
+
+
+class AppTheme{
+  AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TextThemes.lightTextTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TextThemes.darkTextTheme,
+  );
+}
